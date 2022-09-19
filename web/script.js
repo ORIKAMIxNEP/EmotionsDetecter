4eb93697a66d45d2de0043b6d35f5acc1a78f1e3
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-09-19 12:35:23 +0900

    API呼び出し処理の変更

--- web/script.js
+++ web/script.js
@@ -21,16 +21,26 @@ window.onload = () => {
     });
 };
 
-async function test() {
-  let canvas = document.getElementById("canvas");
-  canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
-  let response = await fetch("http://192.168.0.19:51400/", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({
-      imageBase64: canvas.toDataURL().replace(/^data:\w+\/\w+;base64,/, ""),
-    }),
-  });
-  data = await response.json();
-  document.getElementById("message").innerHTML = JSON.stringify(data);
+function startInference() {
+  setInterval(() => {
+    let canvas = document.getElementById("canvas");
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+    fetch("http://192.168.0.19:51400/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        imageBase64: canvas.toDataURL().replace(/^data:\w+\/\w+;base64,/, ""),
+      }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        if (data["happiness"] > 0.2) {
+          document.getElementById("message").innerHTML = "笑顔";
+        } else {
+          document.getElementById("message").innerHTML = "笑顔じゃない";
+        }
+      });
+  }, 2000);
 }
