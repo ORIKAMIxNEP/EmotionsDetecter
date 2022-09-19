3e2773d724b32e2ea6338038bdb44b5073818b50
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-09-19 17:49:25 +0900

    ファイル分割

---
+++ templates/static/DetectEmotions.js
@@ -0,0 +1,40 @@
+let canvas = document.getElementById("canvas");
+let emotionsTypeEnglish = [
+  "anger",
+  "disgust",
+  "fear",
+  "happiness",
+  "sadness",
+  "surprise",
+];
+let emotionsTypeJapanese = ["怒り", "嫌悪", "恐怖", "笑顔", "悲しみ", "驚き"];
+let maxEmotion = { emotion: null, value: 0 };
+
+function DetectEmotions() {
+  setInterval(() => {
+    canvas.getContext("2d").drawImage(video, 0, 0, canvas.width, canvas.height);
+    fetch("http://192.168.0.19/api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        imageBase64: canvas.toDataURL().replace(/^data:\w+\/\w+;base64,/, ""),
+      }),
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        maxEmotion.value = 0;
+        for (let i = 0; i < emotionsTypeEnglish.length; i++) {
+          if (data[emotionsTypeEnglish[i]] > maxEmotion.value) {
+            maxEmotion.emotion = emotionsTypeJapanese[i];
+            maxEmotion.value = data[emotionsTypeEnglish[i]];
+          }
+        }
+        if (data.neutral - 0.4 > maxEmotion.value) {
+          maxEmotion.emotion = "真顔";
+        }
+        document.getElementById("message").innerHTML = maxEmotion.emotion;
+      });
+  }, 2000);
+}
