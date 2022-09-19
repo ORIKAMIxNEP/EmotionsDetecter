e0128c424e51114e01cd084ff6e2bdbfc451e2bc
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-09-19 10:33:19 +0900

    読み込む画像ファイル名変更

--- python/DetectEmotions.py
+++ python/DetectEmotions.py
@@ -12,6 +12,6 @@ detector = Detector(
 def DetectEmotions():
     startTime = time.time()
     EmotionsData = detector.detect_image(
-        "test.png", batch_size=10).emotions.to_dict("dict")
+        "image.png", batch_size=10).emotions.to_dict("dict")
     print("実行時間：" + str(time.time() - startTime))
     return EmotionsData
