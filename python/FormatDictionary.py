5199d889acf87acc582672330e07608b8b4298a2
ORIKAMI-NEP <ta.orikami@gmail.com>
2022-10-31 00:05:57 +0900

    例外処理に対応

--- python/FormatDictionary.py
+++ python/FormatDictionary.py
@@ -1,5 +1,6 @@
 def FormatDictionary(dictionary):
-    for key, value in dictionary.items():
-        dictionary[key] = value[0]
+    if dictionary["anger"] != "nan":
+        for key, value in dictionary.items():
+            dictionary[key] = value[0]
     print(dictionary)
     return dictionary
